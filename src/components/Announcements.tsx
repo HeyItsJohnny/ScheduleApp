8b5ff8f81e9672743a1318c687e4ce036db60066
Jonathan Laroco <jonathan.laroco@gmail.com>
2024-10-16 10:59:15 -0700

    Finishing Admin Page

---
+++ src/components/Announcements.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Image from "next/image";
+
+const Announcements = () => {
+  return (
+    <div className="bg-white p-4 rounded-md">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold my-4">Announcements</h1>
+        <span className="text-xs text-gray-400">View All</span>
+      </div>
+      <div className="flex flex-col gap-4 mt-4">
+        <div className="bg-lamaSkyLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Random Text 1</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+              2025-01-01
+            </span>
+          </div>
+          <p className="text-sm text-gray-400">
+            This is a random text. This is a random text. This is a ransom text.
+          </p>
+        </div>
+        <div className="bg-lamaPurpleLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Random Text 2</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+              2025-01-01
+            </span>
+          </div>
+          <p className="text-sm text-gray-400">
+            This is a random text. This is a random text. This is a ransom text.
+          </p>
+        </div>
+        <div className="bg-lamaYellowLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Random Text 3</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">
+              2025-01-01
+            </span>
+          </div>
+          <p className="text-sm text-gray-400">
+            This is a random text. This is a random text. This is a ransom text.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Announcements;
